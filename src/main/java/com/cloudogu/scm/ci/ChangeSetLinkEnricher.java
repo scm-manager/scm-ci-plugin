cbc46bbd1e98f19448f3038a7331e6b7f7926baa
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-07-19 09:13:53 +0200

    fix embedded enricher

--- src/main/java/com/cloudogu/scm/ci/ChangeSetLinkEnricher.java
+++ src/main/java/com/cloudogu/scm/ci/ChangeSetLinkEnricher.java
@@ -1,13 +1,10 @@
 package com.cloudogu.scm.ci;
 
 import com.cloudogu.scm.ci.cistatus.api.CIStatusDtoCollection;
+import com.cloudogu.scm.ci.cistatus.api.CIStatusMapper;
 import com.cloudogu.scm.ci.cistatus.api.CIStatusResource;
 import com.cloudogu.scm.ci.cistatus.api.CIStatusRootResource;
-import com.cloudogu.scm.ci.cistatus.service.CIStatus;
-import com.cloudogu.scm.ci.cistatus.service.CIStatusCollection;
 import com.cloudogu.scm.ci.cistatus.service.CIStatusService;
-import de.otto.edison.hal.Embedded;
-import de.otto.edison.hal.HalRepresentation;
 import sonia.scm.api.v2.resources.Enrich;
 import sonia.scm.api.v2.resources.HalAppender;
 import sonia.scm.api.v2.resources.HalEnricher;
@@ -21,6 +18,8 @@ import sonia.scm.repository.Repository;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
+import java.util.stream.Collectors;
+
 import static com.cloudogu.scm.ci.PermissionCheck.mayRead;
 
 @Extension
@@ -28,12 +27,14 @@ import static com.cloudogu.scm.ci.PermissionCheck.mayRead;
 public class ChangeSetLinkEnricher implements HalEnricher {
 
   private final Provider<ScmPathInfoStore> scmPathInfoStore;
-  private final CIStatusRootResource ciStatusRootResource;
+  private final CIStatusService ciStatusService;
+  private final CIStatusMapper mapper;
 
   @Inject
-  public ChangeSetLinkEnricher(Provider<ScmPathInfoStore> scmPathInfoStore, CIStatusRootResource ciStatusRootResource) {
+  public ChangeSetLinkEnricher(Provider<ScmPathInfoStore> scmPathInfoStore, CIStatusService ciStatusService, CIStatusMapper mapper) {
     this.scmPathInfoStore = scmPathInfoStore;
-    this.ciStatusRootResource = ciStatusRootResource;
+    this.ciStatusService = ciStatusService;
+    this.mapper = mapper;
   }
 
   @Override
@@ -44,7 +45,12 @@ public class ChangeSetLinkEnricher implements HalEnricher {
       if (mayRead(repository)) {
         LinkBuilder linkBuilder = new LinkBuilder(scmPathInfoStore.get().get(), CIStatusRootResource.class, CIStatusResource.class);
         appender.appendLink("ciStatus", linkBuilder.method("getCIStatusResource").parameters(repository.getNamespace(), repository.getName(), changeset.getId()).method("getAll").parameters().href());
-        appender.appendEmbedded("ciStatus", ciStatusRootResource.getCIStatusResource(repository.getNamespace(), repository.getName(), changeset.getId()).getAll());
+        appender.appendEmbedded("ciStatus",
+          new CIStatusDtoCollection(
+            ciStatusService.get(repository, changeset.getId())
+              .stream()
+              .map(ciStatus -> mapper.map(repository, changeset.getId(), ciStatus))
+              .collect(Collectors.toList())));
       }
   }
 }
