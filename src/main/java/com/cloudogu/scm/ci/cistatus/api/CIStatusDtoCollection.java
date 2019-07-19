24685d3e05356286c2abe728c5d66b16123b840f
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-07-19 09:13:53 +0200

    fix embedded enricher

--- src/main/java/com/cloudogu/scm/ci/cistatus/api/CIStatusDtoCollection.java
+++ src/main/java/com/cloudogu/scm/ci/cistatus/api/CIStatusDtoCollection.java
@@ -2,10 +2,14 @@ package com.cloudogu.scm.ci.cistatus.api;
 
 import de.otto.edison.hal.HalRepresentation;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Collection;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class CIStatusDtoCollection extends HalRepresentation {
   private Collection<CIStatusDto> ciStatusDtos;
 }
