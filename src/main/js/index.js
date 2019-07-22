870c29945cc934622effb8e766bed3bb2dbd990e
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-07-22 14:33:40 +0200

    rename extension

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -4,6 +4,6 @@ import { binder } from "@scm-manager/ui-extensions";
 import CIStatusSummary from "./CIStatusSummary";
 import CIStatusModalView from "./CIStatusModalView";
 
-binder.bind("changeset.modal", CIStatusModalView);
+binder.bind("ciPlugin.modal", CIStatusModalView);
 binder.bind("changeset.right", CIStatusSummary);
 
