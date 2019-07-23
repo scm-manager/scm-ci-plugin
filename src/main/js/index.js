c6403c7128f31b265e1fb95a77af0f09bcbeab2a
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-07-23 15:45:40 +0200

    fix Test / fix extensionpoint name

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -2,9 +2,7 @@
 import React from "react";
 import { binder } from "@scm-manager/ui-extensions";
 import CIStatusSummary from "./CIStatusSummary";
-import CIStatusModalView from "./CIStatusModalView";
 import CIStatusBar from "./CIStatusBar";
 
-binder.bind("changeset.information", CIStatusModalView);
 binder.bind("changeset.right", CIStatusSummary);
-binder.bind("changeset.status", CIStatusBar);
+binder.bind("reviewPlugin.pullrequest.top", CIStatusBar);
--- src/test/java/com/cloudogu/scm/ci/cistatus/api/CIStatusRootResourceTest.java
+++ src/test/java/com/cloudogu/scm/ci/cistatus/api/CIStatusRootResourceTest.java
@@ -2,14 +2,17 @@ package com.cloudogu.scm.ci.cistatus.api;
 
 import com.cloudogu.scm.ci.RepositoryResolver;
 import com.cloudogu.scm.ci.cistatus.service.CIStatusService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.OngoingStubbing;
+import org.mockito.junit.jupiter.MockitoSettings;
+import sonia.scm.repository.Changeset;
 import sonia.scm.repository.Repository;
 import sonia.scm.repository.RepositoryTestData;
+import sonia.scm.repository.api.LogCommandBuilder;
 import sonia.scm.repository.api.RepositoryService;
 import sonia.scm.repository.api.RepositoryServiceFactory;
 
@@ -17,6 +20,7 @@ import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,6 +39,9 @@ class CIStatusRootResourceTest {
   private RepositoryServiceFactory repositoryServiceFactory;
 
   @Mock
+  private LogCommandBuilder logCommandBuilder;
+
+  @Mock
   private RepositoryService repositoryService;
 
   @InjectMocks
@@ -43,14 +50,16 @@ class CIStatusRootResourceTest {
   @Test
   void shouldReturnCiStatusResource() throws IOException {
     Repository repository = RepositoryTestData.createHeartOfGold();
+    Changeset changeset = new Changeset();
+    changeset.setId("42");
     when(resolver.resolve("hitchhiker", "heart-of-gold")).thenReturn(repository);
     when(repositoryServiceFactory.create(any(Repository.class))).thenReturn(repositoryService);
-    when(repositoryService.getLogCommand().getChangeset("42").getId()).thenReturn("42");
+    when(repositoryService.getLogCommand()).thenReturn(logCommandBuilder);
+    when(logCommandBuilder.getChangeset("42")).thenReturn(changeset);
 
     CIStatusResource resource = rootResource.getCIStatusResource("hitchhiker", "heart-of-gold", "42");
 
     assertThat(resource.getRepository()).isSameAs(repository);
     assertThat(resource.getChangesetId()).isEqualTo("42");
   }
-
 }
