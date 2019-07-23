8f944b8629c074bb61b3f7d1ba1b7745c0555da5
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2019-07-23 09:21:53 +0200

    add statusbar

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -3,7 +3,8 @@ import React from "react";
 import { binder } from "@scm-manager/ui-extensions";
 import CIStatusSummary from "./CIStatusSummary";
 import CIStatusModalView from "./CIStatusModalView";
+import CIStatusBar from "./CIStatusBar";
 
 binder.bind("changeset.information", CIStatusModalView);
 binder.bind("changeset.right", CIStatusSummary);
-
+binder.bind("changeset.status", CIStatusBar)
