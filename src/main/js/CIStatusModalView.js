0c7526077e6c4a346b2dd31a63b8415329bcf54c
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-07-25 10:28:46 +0200

    Reuse icons from StatusIcon.js

--- src/main/js/CIStatusModalView.js
+++ src/main/js/CIStatusModalView.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { Modal } from "@scm-manager/ui-components";
 import { translate } from "react-i18next";
-import StatusIcon from "./StatusIcon";
+import {FailureIcon, PlaceholderIcon, SuccessIcon, UnstableIcon} from "./StatusIcon";
 import ModalRow from "./ModalRow";
 
 type Props = {
@@ -25,20 +25,20 @@ class CIStatusModalView extends React.Component<Props> {
           {ciStatus.map(ci =>
             ci.status === "SUCCESS" ? (
               <ModalRow
-                status={<StatusIcon color="success" size="1" icon="check-circle" title={ci.type + ": " + ci.name} titleColor="white" />}
+                status={<SuccessIcon title={ci.type + ": " + ci.name} />}
                 ciUrl={ci.url}
               />):
             ci.status === "FAILURE" ? (
               <ModalRow
-                status={<StatusIcon color="danger" size="1" icon="times-circle" title={ci.type + ": " + ci.name} />}
+                status={<FailureIcon title={ci.type + ": " + ci.name} />}
                 ciUrl={ci.url}
               />):
             ci.status === "UNSTABLE" ? (
               <ModalRow
-                status={<StatusIcon color="warning" size="1" icon="exclamation-circle" title={ci.type + ": " + ci.name} />}
+                status={<UnstableIcon title={ci.type + ": " + ci.name} />}
                 ciUrl={ci.url} />):
             (<ModalRow
-              status={<StatusIcon color="light" size="1" icon="circle-notch" title={ci.type + ": " + ci.name} />}
+              status={<PlaceholderIcon title={ci.type + ": " + ci.name} />}
               ciUrl={ci.url}
               />
               ))}
--- src/main/js/StatusIcon.js
+++ src/main/js/StatusIcon.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React  from "react";
 
-type Props = {
+
+type BaseProps = {
+  title?: string,
+  titleColor?: string
+};
+
+type Props = BaseProps & {
   color: string,
   icon : string,
-  size: string,
-  title?: string
+  size: string
 };
 
 class StatusIcon extends React.Component<Props> {
@@ -19,13 +24,11 @@ class StatusIcon extends React.Component<Props> {
   }
 }
 
-export const SuccessIcon = () => <StatusIcon color="success" icon="check-circle" size="1"/>;
-export const FailureIcon = () => <StatusIcon color="danger" icon="times-circle" size="1"/>;
-export const UnstableIcon = () => <StatusIcon color="warning" icon="exclamation-circle" size="1" />;
+export const SuccessIcon: React.SFC<BaseProps> = (props) => <StatusIcon color="success" icon="check-circle" size="1" {...props} />;
+export const FailureIcon: React.SFC<BaseProps> = (props) => <StatusIcon color="danger" icon="times-circle" size="1" {...props}/>;
+export const UnstableIcon: React.SFC<BaseProps> = (props) => <StatusIcon color="warning" icon="exclamation-circle" size="1" {...props} />;
 
-export const PlaceholderIcon = () => <StatusIcon color="light" icon="circle-notch" size="1" />;
-export const AbortedIcon = PlaceholderIcon;
-export const PendingIcon = PlaceholderIcon;
+export const PlaceholderIcon: React.SFC<BaseProps> = (props) => <StatusIcon color="light" icon="circle-notch" size="1" {...props} />;
 
 export default StatusIcon;
 
