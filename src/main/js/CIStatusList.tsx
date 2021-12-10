756574eadca71990369ae8eced30eeb43a3da65b
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2021-12-10 16:07:45 +0100

    Use popover style from review plugin

---
+++ src/main/js/CIStatusList.tsx
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import React, { FC } from "react";
+import StatusIcon, { SuccessIcon, FailureIcon, UnstableIcon } from "./StatusIcon";
+import ModalRow from "./ModalRow";
+import { CIStatus, getDisplayName } from "./CIStatus";
+
+type Props = {
+  ciStatus: CIStatus;
+};
+
+const CIStatusList: FC<Props> = ({ ciStatus }) => {
+  return (
+    <>
+      {ciStatus.map(ci =>
+        ci.status === "SUCCESS" ? (
+          <ModalRow status={<SuccessIcon titleType={ci.type} title={getDisplayName(ci)} size="lg"/>} ciUrl={ci.url}/>
+        ) : ci.status === "FAILURE" ? (
+          <ModalRow
+            status={<FailureIcon titleType={ci.type} title={getDisplayName(ci)} size="lg"/>}
+            ciUrl={ci.url}
+          />
+        ) : ci.status === "UNSTABLE" ? (
+          <ModalRow
+            status={<UnstableIcon titleType={ci.type} title={getDisplayName(ci)} size="lg"/>}
+            ciUrl={ci.url}
+          />
+        ) : (
+          <ModalRow status={<StatusIcon titleType={ci.type} title={getDisplayName(ci)} size="lg"/>} ciUrl={ci.url}/>
+        )
+      )}
+    </>
+  );
+};
+
+export default CIStatusList;
--- src/main/js/CIStatusModalView.tsx
+++ src/main/js/CIStatusModalView.tsx
@@ -24,10 +24,9 @@
 import React from "react";
 import { withTranslation, WithTranslation } from "react-i18next";
 import { Modal } from "@scm-manager/ui-components";
-import StatusIcon, { SuccessIcon, FailureIcon, UnstableIcon } from "./StatusIcon";
-import ModalRow from "./ModalRow";
-import { CIStatus, getDisplayName } from "./CIStatus";
+import { CIStatus } from "./CIStatus";
 import { getColor } from "./StatusIcon";
+import CIStatusList from "./CIStatusList";
 
 type Props = WithTranslation & {
   ciStatus: CIStatus;
@@ -38,30 +37,7 @@ class CIStatusModalView extends React.Component<Props> {
   render() {
     const { onClose, ciStatus, t } = this.props;
 
-    const body = (
-      <>
-        {ciStatus.map(ci =>
-          ci.status === "SUCCESS" ? (
-            <ModalRow
-              status={<SuccessIcon titleType={ci.type} title={getDisplayName(ci)} size="lg" />}
-              ciUrl={ci.url}
-            />
-          ) : ci.status === "FAILURE" ? (
-            <ModalRow
-              status={<FailureIcon titleType={ci.type} title={getDisplayName(ci)} size="lg" />}
-              ciUrl={ci.url}
-            />
-          ) : ci.status === "UNSTABLE" ? (
-            <ModalRow
-              status={<UnstableIcon titleType={ci.type} title={getDisplayName(ci)} size="lg" />}
-              ciUrl={ci.url}
-            />
-          ) : (
-            <ModalRow status={<StatusIcon titleType={ci.type} title={getDisplayName(ci)} size="lg" />} ciUrl={ci.url} />
-          )
-        )}
-      </>
-    );
+    const body = <CIStatusList ciStatus={ciStatus} />;
     const errors =
       ciStatus && ciStatus.length > 0
         ? ciStatus.filter(ci => ci.status === "FAILURE" || ci.status === "UNSTABLE").length
