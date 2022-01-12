e313da184296457ea1bd2d18ea3cc7ded155d6d5
Konstantin Schaper <konstantin.schaper@cloudogu.com>
2022-01-12 13:14:15 +0100

    fix ci status error for un-open pull requests

--- src/main/js/CIStatus.ts
+++ src/main/js/CIStatus.ts
@@ -24,31 +24,6 @@
 
 import { apiClient } from "@scm-manager/ui-components";
 import { Branch, Link, Repository } from "@scm-manager/ui-types";
-
-/*
- * MIT License
- *
- * Copyright (c) 2020-present Cloudogu GmbH and Contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 import { useQuery } from "react-query";
 
 export type CIStatus = {
@@ -71,6 +46,13 @@ export const useCiStatus = (
   context: CiStatusContext,
   callback: (ciStatus: CIStatus[]) => void
 ) => {
+  let url: string | undefined;
+  if (context.pullRequest) {
+    url = (context.pullRequest._links.ciStatus as Link)?.href;
+  } else if (context.branch) {
+    url = (context.branch._links.details as Link)?.href;
+  }
+
   const { error, isLoading, data } = useQuery<CIStatus[], Error>(
     [
       "repository",
@@ -80,27 +62,17 @@ export const useCiStatus = (
       context.pullRequest ? "pull-request" : "branch",
       context.pullRequest?.id || context.branch?.name
     ],
-    () => {
-      if (context.pullRequest) {
-        return apiClient
-          .get((context.pullRequest._links.ciStatus as Link)?.href)
-          .then(response => response.json())
-          .then(json => json._embedded.ciStatus)
-          .then(ciStatus => {
-            callback(ciStatus);
-            return ciStatus;
-          });
-      } else if (context.branch) {
-        return apiClient
-          .get((context.branch._links.details as Link)?.href)
-          .then(response => response.json())
-          .then(json => json._embedded.ciStatus)
-          .then(ciStatus => {
-            callback(ciStatus);
-            return ciStatus;
-          });
-      }
-      return [];
+    () =>
+      apiClient
+        .get(url!)
+        .then(response => response.json())
+        .then(json => json._embedded.ciStatus)
+        .then(ciStatus => {
+          callback(ciStatus);
+          return ciStatus;
+        }),
+    {
+      enabled: !!url
     }
   );
 
